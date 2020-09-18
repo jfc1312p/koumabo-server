ab11eccbb80de837aa4762bfb571bf74aedaca6a
jfc1312p <jfc1312@gmail.com>
2020-09-18 14:22:25 -0400

    development boiler

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const app =  express()
-const authRoutes = require('./routes/auth')
-//
+const app = express();
 
+// connect to db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log('DB CONNECTION ERROR: ', err));
 
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
-//Route middleware
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = 'development')) {
+    app.use(cors({ origin: `http://localhost:3000` }));
+}
 
-app.use(authRoutes)
+// middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
 
-
-const port = process.env.PORT || 8000
-
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`API is running on port ${port}`)
-})
+    console.log(`API is running on port ${port}`);
+});
